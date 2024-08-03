2316282bb406005d421400b378c01b2cabcf8289
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-08-02 18:01:52 -0700

    Display product image as div background

--- src/App.js
+++ src/App.js
@@ -31,10 +31,15 @@ const App = () => {
 
   return (
     <div className="categories-container">
-      {categories.map(({ title, id }) => {
+      {categories.map(({ title, id, imageUrl }) => {
         return (
           <div key={id} className="category-container">
-            <div className="background-image"></div>
+            <div
+              className="background-image"
+              style={{
+                backgroundImage: `url(${imageUrl})`,
+              }}
+            />
             <div className="category-body-container">
               <h2>{title}</h2>
               <p>Shop Now</p>
