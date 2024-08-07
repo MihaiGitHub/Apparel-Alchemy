248a97397fac41db146433d1107e0e16dfd3d020
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-08-07 11:12:49 -0700

    Refactor Navigation into separate file

--- src/App.js
+++ src/App.js
@@ -1,16 +1,6 @@
-import { Routes, Route, Outlet } from "react-router-dom";
-import Home from "./routes/home.component";
-
-const Navigation = () => {
-  return (
-    <div>
-      <div>
-        <h1>I am the Navigation bar</h1>
-      </div>
-      <Outlet />
-    </div>
-  );
-};
+import { Routes, Route } from "react-router-dom";
+import Home from "./routes/home/home.component";
+import Navigation from "./routes/navigation/navigation.component";
 
 const Shop = () => {
   return (
