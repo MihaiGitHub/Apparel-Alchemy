316f91455cce632c7c476f511731577c0f1af0d8
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-09-07 20:22:50 -0700

    Update json data

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -6,26 +6,31 @@ const Home = () => {
       id: 1,
       title: "hats",
       imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
+      alt: "hats",
     },
     {
       id: 2,
       title: "jackets",
       imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
+      alt: "jackets",
     },
     {
       id: 3,
       title: "sneakers",
       imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
+      alt: "sneakers",
     },
     {
       id: 4,
       title: "womens",
       imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
+      alt: "womens",
     },
     {
       id: 5,
       title: "mens",
       imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
+      alt: "mens",
     },
   ];
 
