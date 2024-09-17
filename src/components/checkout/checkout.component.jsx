d797468b4d2e7c0b8bc98aa0c8592d22be0b001e
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-09-17 10:20:44 -0700

    Increment item in checkout feature

--- src/components/checkout/checkout.component.jsx
+++ src/components/checkout/checkout.component.jsx
@@ -1,7 +1,32 @@
+import { useContext } from "react";
+import { CartContext } from "../../contexts/cart.context";
+
 import "./checkout.styles.scss";
 
 const Checkout = () => {
-  return <div>Checkout page</div>;
+  const { cartItems, addItemToCart } = useContext(CartContext);
+
+  return (
+    <div>
+      <h1>Checkout</h1>
+      <div>
+        {cartItems.map((cartItem) => {
+          const { id, name, quantity } = cartItem;
+
+          return (
+            <div key={id}>
+              <h2>{name}</h2>
+              <span>{quantity}</span>
+              <span>decrement</span>
+              <br />
+              <span onClick={() => addItemToCart(cartItem)}>increment</span>
+              <br />
+            </div>
+          );
+        })}
+      </div>
+    </div>
+  );
 };
 
 export default Checkout;
