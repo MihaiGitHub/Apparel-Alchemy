ecd3933f11243ade103d01d591b0e0d6ad638048
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-10-29 14:07:34 +0800

    Configure category routes

--- src/routes/home/home.component.jsx
+++ src/routes/home/home.component.jsx
@@ -1,40 +1,7 @@
 import Directory from "../../components/directory/directory.component";
 
 const Home = () => {
-  const categories = [
-    {
-      id: 1,
-      title: "hats",
-      imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-      alt: "hats",
-    },
-    {
-      id: 2,
-      title: "jackets",
-      imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-      alt: "jackets",
-    },
-    {
-      id: 3,
-      title: "sneakers",
-      imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-      alt: "sneakers",
-    },
-    {
-      id: 4,
-      title: "womens",
-      imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-      alt: "womens",
-    },
-    {
-      id: 5,
-      title: "mens",
-      imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-      alt: "mens",
-    },
-  ];
-
-  return <Directory categories={categories} />;
+  return <Directory />;
 };
 
 export default Home;
