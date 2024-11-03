ca5bb648b82490306f0c013b8e1b26d643cc8c28
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-11-03 16:49:00 +0800

    Convert directory to styled components

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -1,52 +1,14 @@
 import DirectoryItem from "../directory-item/directory-item.component";
 
-import "./directory.styles.scss";
+import { DirectoryContainer } from "./directory.styles";
 
-const categories = [
-  {
-    id: 1,
-    title: "hats",
-    imageUrl: "https://i.ibb.co/cvpntL1/hats.png",
-    alt: "hats",
-    route: "shop/hats",
-  },
-  {
-    id: 2,
-    title: "jackets",
-    imageUrl: "https://i.ibb.co/px2tCc3/jackets.png",
-    alt: "jackets",
-    route: "shop/jackets",
-  },
-  {
-    id: 3,
-    title: "sneakers",
-    imageUrl: "https://i.ibb.co/0jqHpnp/sneakers.png",
-    alt: "sneakers",
-    route: "shop/sneakers",
-  },
-  {
-    id: 4,
-    title: "womens",
-    imageUrl: "https://i.ibb.co/GCCdy8t/womens.png",
-    alt: "womens",
-    route: "shop/womens",
-  },
-  {
-    id: 5,
-    title: "mens",
-    imageUrl: "https://i.ibb.co/R70vBrQ/men.png",
-    alt: "mens",
-    route: "shop/mens",
-  },
-];
-
-const Directory = () => {
+const Directory = ({ categories }) => {
   return (
-    <div className="directory-container">
+    <DirectoryContainer>
       {categories.map((category) => (
         <DirectoryItem key={category.id} category={category} />
       ))}
-    </div>
+    </DirectoryContainer>
   );
 };
 
