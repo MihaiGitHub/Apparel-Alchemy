4f1cd3e9c6f2459fc931682519f6150cd7a4a35a
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-11-24 10:33:02 -0700

    Create userReducer

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -1,18 +1,45 @@
-import { createContext, useState, useEffect } from "react";
+import { createContext, useEffect, useReducer } from "react";
 import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
 
-// the actual value you want to access
+// the actual values you want to access (expose to the rest of the app)
 export const UserContext = createContext({
   currentUser: null,
   setCurrentUser: () => null,
 });
 
+export const USER_ACTION_TYPES = {
+  SET_CURRENT_USER: "SET_CURRENT_USER",
+};
+
+// reducers scale well
+// with a reducer, when the dispatch gets called, and a new state object is returned, then this functional component rerenders (similar to setState updating components)
+const userReducer = (state, action) => {
+  const { type, payload } = action;
+
+  switch (type) {
+    case USER_ACTION_TYPES.SET_CURRENT_USER:
+      return {
+        currentUser: payload,
+      };
+    default:
+      throw new Error(`Unhandled type ${type} in userReducer`);
+  }
+};
+
+const INITIAL_STATE = {
+  currentUser: null,
+};
+
 // provider component that allows child components to access the values inside its useState
 export const UserProvider = ({ children }) => {
-  const [currentUser, setCurrentUser] = useState(null);
+  const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
+  const setCurrentUser = (user) => {
+    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+  };
+
   const value = { currentUser, setCurrentUser };
 
   useEffect(() => {
