3bcc72d0cc4f87cc11f82657e7946786fa9a668d
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-11-26 10:40:05 -0700

    Optimize user context with createAction utils function

--- src/contexts/user.context.jsx
+++ src/contexts/user.context.jsx
@@ -3,6 +3,7 @@ import {
   onAuthStateChangedListener,
   createUserDocumentFromAuth,
 } from "../utils/firebase/firebase.utils";
+import { createAction } from "../utils/reducer/reducer.utils";
 
 // the actual values you want to access (expose to the rest of the app)
 export const UserContext = createContext({
@@ -37,7 +38,7 @@ const INITIAL_STATE = {
 export const UserProvider = ({ children }) => {
   const [{ currentUser }, dispatch] = useReducer(userReducer, INITIAL_STATE);
   const setCurrentUser = (user) => {
-    dispatch({ type: USER_ACTION_TYPES.SET_CURRENT_USER, payload: user });
+    dispatch(createAction(USER_ACTION_TYPES.SET_CURRENT_USER, user));
   };
 
   const value = { currentUser, setCurrentUser };
