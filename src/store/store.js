1814615cb9ad715570cc6429fa84407611882a3c
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-11-29 10:51:43 -0700

    Setup Redux store

--- src/store/store.js
+++ src/store/store.js
@@ -1,2 +1,11 @@
 import { compose, createStore, applyMiddleware } from "redux";
 import logger from "redux-logger";
+import { rootReducer } from "./root-reducer";
+
+// runs before the action hits the reducer
+const middleWares = [logger];
+
+const composedEnhancers = compose(applyMiddleware(...middleWares));
+
+// undefined is an optional second parameter
+export const store = createStore(rootReducer, undefined, composedEnhancers);
