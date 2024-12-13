f3d3dadde6448274c163746e93a4e29710130a7d
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-12-13 10:34:04 -0700

    Setup redux persist

--- src/store/store.js
+++ src/store/store.js
@@ -1,4 +1,6 @@
 import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer } from "redux-persist";
+import storage from "redux-persist/lib/storage";
 // import logger from "redux-logger";
 import { rootReducer } from "./root-reducer";
 
@@ -15,10 +17,24 @@ const loggerMiddleware = (store) => (next) => (action) => {
   console.log("next state: ", store.getState());
 };
 
+const persistConfig = {
+  key: "root",
+  storage,
+  blacklist: ["user"], // values of which reducer you DON'T want to persist
+};
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
 // runs before the action hits the reducer
 const middleWares = [loggerMiddleware];
 
 const composedEnhancers = compose(applyMiddleware(...middleWares));
 
 // undefined is an optional second parameter
-export const store = createStore(rootReducer, undefined, composedEnhancers);
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+export const persistor = persistStore(store);
