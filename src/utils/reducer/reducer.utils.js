bafadd773f93d7f6b8ae1fc66ac6e3b760981832
Mihai Smarandache <mihai.sanfran@gmail.com>
2024-12-24 17:29:54 -0700

    Create signInWithGoogle saga

--- src/utils/reducer/reducer.utils.js
+++ src/utils/reducer/reducer.utils.js
@@ -1,1 +1,5 @@
-export const createAction = (type, payload) => ({ type, payload });
+// export const createAction = (type, payload) => ({ type, payload });
+export const createAction = (type, payload) => {
+  console.log("create action");
+  return { type, payload };
+};
