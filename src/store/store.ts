4f954cbb12f4ed59ef01110e4eba920a07ca94dc
mihai <mihai.sanfran@gmail.com>
2025-02-28 12:10:51 -0700

    Type out store

---
+++ src/store/store.ts
@@ -0,0 +1,58 @@
+import { compose, createStore, applyMiddleware } from "redux";
+import { persistStore, persistReducer, PersistConfig } from "redux-persist";
+import storage from "redux-persist/lib/storage";
+import logger from "redux-logger";
+import createSagaMiddleware from "redux-saga";
+import { rootSaga } from "./root-saga";
+import { rootReducer } from "./root-reducer";
+
+// typeof gives you the type of rootReducer whatever it is
+export type RootState = ReturnType<typeof rootReducer>;
+
+declare global {
+  interface Window {
+    __REDUX_DEVTOOLS_EXTENSION_COMPOSE__?: typeof compose;
+  }
+}
+
+//PersistConfig is a type that contains all the existing values that exist inside of a PersistConfig so we want to extend on this
+type ExtendedPersistConfig = PersistConfig<RootState> & {
+  whitelist: (keyof RootState)[];
+};
+
+const persistConfig: ExtendedPersistConfig = {
+  key: "root",
+  storage,
+  whitelist: ["cart"],
+};
+
+const sagaMiddleware = createSagaMiddleware();
+
+const persistedReducer = persistReducer(persistConfig, rootReducer);
+
+// const middleWares = [
+//   process.env.NODE_ENV !== "production" && logger,
+//   sagaMiddleware,
+// ].filter((middleware): middleware is Middleware => Boolean(middleware));
+const middleWares = [
+  process.env.NODE_ENV !== "production" && logger,
+  sagaMiddleware,
+].filter(Boolean);
+
+const composeEnhancer =
+  (process.env.NODE_ENV !== "production" &&
+    window &&
+    window.__REDUX_DEVTOOLS_EXTENSION_COMPOSE__) ||
+  compose;
+
+const composedEnhancers = composeEnhancer(applyMiddleware(...middleWares));
+
+export const store = createStore(
+  persistedReducer,
+  undefined,
+  composedEnhancers
+);
+
+sagaMiddleware.run(rootSaga);
+
+export const persistor = persistStore(store);
